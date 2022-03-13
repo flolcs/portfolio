31ff8b98cd0375f89011518bba910c15038e1cd5
FloLcs <florianlcs16@gmail.com>
2022-03-13 17:21:09 +0100

    music et debut skill js

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ const mySong = document.querySelector("#mySong");
 music.addEventListener("click", () => {
   if (mySong.paused) {
     mySong.play();
+    music.style.border = "3px solid #f4c095";
+    music.style.borderRadius = "50px";
   } else {
     mySong.pause();
+    music.style.border = "none";
   }
 });
 
@@ -38,3 +41,39 @@ window.onscroll = () => {
     }
   });
 };
+
+// HTML
+const slideHtml = document.querySelector("#sous-container1");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideHtml.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.5) {
+    slideHtml.classList.add("active");
+  }
+});
+
+// CSS
+const slideCss = document.querySelector("#sous-container2");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideCss.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.5) {
+    slideCss.classList.add("active");
+  }
+});
+
+// JS
+const slideJs = document.querySelector("#sous-container3");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideJs.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.5) {
+    slideJs.classList.add("active");
+  }
+});
