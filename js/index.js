b209a3ef1ea1cb767431715a3e424482b8812bf1
FloLcs <florianlcs16@gmail.com>
2022-04-18 11:56:38 +0200

    faire portfolio

---
+++ js/index.js
@@ -0,0 +1,91 @@
+// Music
+const music = document.querySelector("#music-note");
+const mySong = document.querySelector("#mySong");
+
+music.addEventListener("click", () => {
+  if (mySong.paused) {
+    mySong.play();
+    music.style.border = "2px solid #ffffff";
+    music.style.borderRadius = "50px";
+  } else {
+    mySong.pause();
+    music.style.border = "none";
+  }
+});
+
+// Typing
+const typed = new Typed("h2", {
+  strings: ["Développeur Web Front-End"],
+  typeSpeed: 75,
+});
+
+//Navbar
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("header nav ul li");
+let current = "";
+
+//Navbar
+window.onscroll = () => {
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.className === current) {
+      li.classList.add("active");
+    }
+  });
+};
+
+// HTML
+const slideHtml = document.querySelector("#sous-container1");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideHtml.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.5) {
+    slideHtml.classList.add("active");
+  }
+});
+
+// CSS
+const slideCss = document.querySelector("#sous-container2");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideCss.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.5) {
+    slideCss.classList.add("active");
+  }
+});
+
+// JS
+const slideJs = document.querySelector("#sous-container3");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  const topToTop = slideJs.getBoundingClientRect().top;
+
+  if (scrollTop > (scrollTop + topToTop).toFixed() - clientHeight * 0.6) {
+    slideJs.classList.add("active");
+  }
+});
+
+// ScrollReveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2000,
+  delay: 400,
+  //reset: true
+});
+
+sr.reveal("#downloadcv", { delay: 500, origin: "bottom" });
+sr.reveal(".aboutme-description", { origin: "left" });
+sr.reveal("#skills-outils", { origin: "bottom" });
+sr.reveal("#contact-description", { origin: "top" });
